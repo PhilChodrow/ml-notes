cf8fa882a0c7f3bd06e441d3c6c6974d43f778f8
Philip Chodrow <pchodrow@gmail.com>
2024-01-15 12:28:38 -0500

    initial skeleton

---
+++ scripts/create-ipynb.py
@@ -0,0 +1,22 @@
+import os
+import nbformat as nb
+import re
+
+os.makedirs("docs/live-notebooks", exist_ok=True)
+for f in os.listdir("chapters"):
+    if f[-4:] == ".qmd":
+        out_path = f"docs/live-notebooks/{f.replace('qmd', 'ipynb')}"
+        os.system(f"quarto convert chapters/{f} -o {out_path}")
+        
+        notebook = nb.read(out_path, as_version = 4)
+        for cell in notebook["cells"]:
+            cell["source"] = re.sub(r"#-#[\S\s]*?#-#", "", cell["source"])
+        
+        nb.write(notebook, out_path)
+        
+        
+        
+    
+
+    
+
