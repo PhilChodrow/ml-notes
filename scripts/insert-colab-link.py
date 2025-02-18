fd4d78b6b993f398f119909481c74553daaf41ac
Philip Chodrow <pchodrow@gmail.com>
2025-02-18 12:47:46 -0500

    republish?

--- scripts/insert-colab-link.py
+++ scripts/insert-colab-link.py
@@ -9,7 +9,7 @@ for f in os.listdir("docs/chapters"):
             
             notebook_path = f.replace(".html", ".ipynb")
             
-            colab_link_line = f'<p><i><a href="http://colab.research.google.com/github/philchodrow/ml-notes/blob/main/docs/live-notebooks/{notebook_path}">Open the live notebook</a> in Google Colab.</i></p>'
+            colab_link_line = f'<p><i><a href="http://colab.research.google.com/github/philchodrow/ml-notes/blob/main/docs/live-notebooks/{notebook_path}">Open the live notebook</a> in Google Colab or <a href="https://www.philchodrow.prof/ml-notes/live-notebooks/{notebook_path}">download the live notebook</a></i></p>.'
             
             text = file.read()
             text = re.sub(pattern, r'\1\n\n' + colab_link_line, text)
